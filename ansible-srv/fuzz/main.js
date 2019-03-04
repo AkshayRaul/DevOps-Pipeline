9e2e89c224563fde61cbec987ec5357f9b550645
Akshay <araul@ncsu.edu>
2019-03-04 12:05:53 -0500

    testing mutate equals

--- ansible-srv/fuzz/main.js
+++ ansible-srv/fuzz/main.js
@@ -1,68 +1,92 @@
 var Random = require('random-js'),
-    fs = require('fs'),
-    stackTrace = require('stacktrace-parser')
-    ;
+  fs = require('fs'),
+  stackTrace = require('stacktrace-parser')
+  ;
 
-var fuzzer = 
+myController=[
+  "APIPatientController.java"
+]
+
+var fuzzer =
 {
-    random : new Random(Random.engines.mt19937().seed(0)),
-    
-    seed: function (kernel)
-    {
-        fuzzer.random = new Random(Random.engines.mt19937().seed(kernel));
-    },
+  random: new Random(Random.engines.mt19937().seed(0)),
 
-    mutate:
-    {
-        string: function(val)
-        {
-            
-            var array = val.split('');
-	    
-	    // mutate '==' to '!=' 
-            if( fuzzer.random.bool(1) )
-            {
-                for(var i=1;i<array.length;i++){
-		   if(array[i] === '=' && array[i-1]==='='){
-			if(fuzzer.random.bool(.5)){
-		   		array[i-1] = '!';
-			}
-		   }
-		}
+  seed: function (kernel) {
+    fuzzer.random = new Random(Random.engines.mt19937().seed(kernel));
+  },
+
+  mutateEquals:
+  {
+    string: function (val) {
+
+      var array = val.split('');
+
+      // mutate '==' to '!=' 
+      if (fuzzer.random.bool(1)) {
+        for (var i = 1; i < array.length; i++) {
+          if (array[i] === '=' && array[i - 1] === '=') {
+            if (fuzzer.random.bool(.5)) {
+              array[i - 1] = '!';
             }
-	   return array.join('');
+          }
         }
+      }
+      return array.join('');
     }
+  },
+  mutateNumbers:{
+    string: function (val) {
+
+      var array = val.split('');
+
+      // mutate 0 to 1 & vice-versa
+      if (fuzzer.random.bool(1)) {
+        for (var i = 1; i < array.length; i++) {
+          if (array[i] === '0') {
+            if (fuzzer.random.bool(.5)) {
+              array[i] = '1';
+            }
+          }
+          else if (array[i] === '1') {
+            if (fuzzer.random.bool(.5)) {
+              array[i] = '0';
+            }
+          }
+        }
+      }
+      return array.join('');
+    }
+
+  }
 };
 
-if( process.env.NODE_ENV != "test")
-{
-    fuzzer.seed(0);
-    var patientControllerPath = "../iTrust2/iTrustBareGit/iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/APIPatientController.java"
-    mutationTesting([patientControllerPath],1);
+if (process.env.NODE_ENV != "test") {
+  fuzzer.seed(0);
+  var patientControllerPath = "../iTrust2/iTrustBareGit/iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/"+myController[0];
+  mutationTesting([patientControllerPath], 1);
 }
 
-function mutationTesting(paths,iterations)
-{    
-    var failedTests = [];
-    var reducedTests = [];
-    var passedTests = 0;
-	   
-    var markDownA = fs.readFileSync(paths[0],'utf-8');
-    var newString = fuzzer.mutate.string(markDownA);
-    fs.writeFileSync(paths[0], newString, 'utf-8');    
+function mutationTesting(paths, iterations) {
+  var failedTests = [];
+  var reducedTests = [];
+  var passedTests = 0;
+
+  var markDownA = fs.readFileSync(paths[0], 'utf-8');
+  var newString = fuzzer.mutateEquals.string(markDownA);
+  newString = fuzzer.mutateNumbers.string(newString);
+  fs.writeFileSync(paths[0], newString, 'utf-8');
 }
 exports.mutationTesting = mutationTesting;
 exports.fuzzer = fuzzer;
 
 if (!String.prototype.format) {
-  String.prototype.format = function() {
+  String.prototype.format = function () {
     var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
+    return this.replace(/{(\d+)}/g, function (match, number) {
       return typeof args[number] != 'undefined'
         ? args[number]
         : match
-      ;
+        ;
     });
   };
 }
